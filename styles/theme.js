c2e84d0221fd815046ddee4fbf45d498b1414083
Mahadeb Sen <mahadebsen237@gmail.com>
2022-07-17 02:15:41 +0530

    first commit

---
+++ styles/theme.js
@@ -0,0 +1,28 @@
+const theme = {
+  breakpoints: {
+    sm: "375px",
+    md: "768px",
+    lg: "1440px",
+  },
+  colors: {
+    primary: {
+      cyan: "#2acfcf",
+      darkViolet: "#3b3054",
+    },
+    secondary: {
+      red: "#f46262",
+    },
+    neutral: {
+      gray: "#bfbfbf",
+      grayishViolet: "#9e9aa7",
+      veryDarkBlue: "#35323e",
+      veryDarkViolet: "#232127",
+    },
+  },
+  typography: {
+    fontSize: "18px",
+    fontWeights: [500, 700],
+  },
+};
+
+export default theme;
